9728389bead95726709665dd041d5949fe90bf96
Rodrigo Mourão <rmmourao@gmail.com>
2019-07-13 02:59:47 -0300

    Adicionadas atividades aulas 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 0
+do {
+  console.log('Tudo bem?')
+  c++
+} while (c < 6 /*c <= 6*/)
+
+var d = 0
+while (d < 6) {
+  console.log('Tudo bem! E você?')
+  d++
+}
+
+/*
+O código acima representa os códigos abaixo
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+var i = 0
+while (i <= 10) {
+  console.log(`Passo ${i}.`)
+  i++
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,26 @@
+/*
+for(var c = 1; c <=5; c++) {
+  console.log(c)
+}
+
+O código acima representa os códigos abaixo
+var c = 1
+while (c <= 5) {
+  console.log(c)
+  c++
+}
+
+ou
+
+var c = 1
+do {
+  console.log(c)
+  c++
+} while (c <= 5)
+*/
+
+console.log('Vai começar...')
+for(var c = 4; c >= 1; c--) {
+  console.log(c)
+}
+console.log('FIM!')
